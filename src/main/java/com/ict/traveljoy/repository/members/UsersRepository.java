d4a72822ce44d8fb2898191ac70c14003b2e5923
MRY4130 <sjd4154@gmail.com>
2024-07-22 18:27:10 +0900

    gradle 추가, converter추가, users repository 추가
    gradle 추가, converter추가, users repository 추가

---
+++ src/main/java/com/ict/traveljoy/repository/members/UsersRepository.java
@@ -0,0 +1,16 @@
+package com.ict.traveljoy.repository.members;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long>{
+	//중복이메일 검증용
+	boolean existsByEmail(String email);
+	//중복닉네임 검증용
+	boolean existsByNickname(String nickname);
+	//이메일 조회용
+	Optional<Users> findByEmail(String email);
+}
