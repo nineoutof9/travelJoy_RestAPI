c3bb8a19d3103e99a173bc30b9aaf42f7059f361
lama9898 <32102298+lama9898@users.noreply.github.com>
2024-07-22 21:19:21 +0900

    gradle 추가, converter추가, users repository 추가 by 서정덕
    gradle 추가, converter추가, users repository 추가
    
    Co-authored-by: MRY4130 <sjd4154@gmail.com>

---
+++ src/main/java/com/ict/traveljoy/repository/converter/PermissionToNumberConverter.java
@@ -0,0 +1,23 @@
+package com.ict.traveljoy.repository.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class PermissionToNumberConverter implements AttributeConverter<String, Integer> {
+
+	@Override
+	public Integer convertToDatabaseColumn(String attribute) {
+		if (attribute != null && attribute.equals("Admin"))
+            return 1;
+		return 0;
+	}
+
+	@Override
+	public String convertToEntityAttribute(Integer dbData) {
+        if (dbData == 1)
+                return "Admin";
+        return "User";
+	}
+	
+}
