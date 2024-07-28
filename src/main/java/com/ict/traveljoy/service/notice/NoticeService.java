5e39aaa3fff53f775deee12a28547fc51ae4a3fc
lama9898 <32102298+lama9898@users.noreply.github.com>
2024-07-28 12:16:58 +0900

    notice entity dto by HanDongJin
    Co-authored-by: HanDongJin <wjsrnr1994@gmail.com>

---
+++ src/main/java/com/ict/traveljoy/service/notice/NoticeService.java
@@ -0,0 +1,14 @@
+package com.ict.traveljoy.service.notice;
+
+import org.springframework.stereotype.Service;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor 
+@Service
+public class NoticeService {
+	
+	
+	
+}
