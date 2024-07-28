9f3924f93e4b0df2b22021b51f5481af2d4e77f5
lama9898 <32102298+lama9898@users.noreply.github.com>
2024-07-28 18:55:31 +0900

    HanDongJin
    * notice entity dto
    * update controller and etc
    * controller Edit and service
    
    Co-authored-by: HanDongJin <wjsrnr1994@gmail.com>

--- src/main/java/com/ict/traveljoy/service/notice/NoticeService.java
+++ src/main/java/com/ict/traveljoy/service/notice/NoticeService.java
@@ -1,14 +1,37 @@
 package com.ict.traveljoy.service.notice;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
-
-
+import org.springframework.transaction.annotation.Transactional;
+import com.ict.traveljoy.repository.notice.Notice;
+import com.ict.traveljoy.repository.notice.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor 
 @Service
+@RequiredArgsConstructor 
 public class NoticeService {
 	
+
+	private final NoticeRepository noticeRepository;
 	
+	@Transactional
+	public NoticeDto createNotice(NoticeDto noticeDto) {
+		Notice notice= noticeRepository.save(noticeDto.toEntity());
+		return NoticeDto.toDto(notice);
+	}
+	
+	@Transactional
+	public List<NoticeDto> findAll(NoticeDto noticeDto) {
+		return null;
+	}
+
+	@Transactional
+	public NoticeDto noticeUpdate(NoticeDto noticeDto) {
+		return null;
+	}
+
+	public NoticeDto noticeDelete(NoticeDto noticeDto) {
+		return null;
+	}
 	
 }
