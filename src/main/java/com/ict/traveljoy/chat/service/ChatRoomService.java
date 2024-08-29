c81a760d9f003be6247cb9691b36161a5d73ea51
lama9898 <32102298+lama9898@users.noreply.github.com>
2024-08-29 18:04:51 +0900

    [Fix, Feat] Chatting Service, Chatroom Admin Check (#61)
    * [Fix] MessageService
    
    * [Feat] Connect Chatroom Admin

--- src/main/java/com/ict/traveljoy/chat/service/ChatRoomService.java
+++ src/main/java/com/ict/traveljoy/chat/service/ChatRoomService.java
@@ -27,6 +27,11 @@ public class ChatRoomService {
 	
 	
 	public String getChatRoomTopic(String useremail) {
+		
+		if(useremail==null || !userRepository.existsByEmail(useremail)) {
+			return "temp";
+		}
+		
 		Users user = userRepository.findByEmail(useremail).get();
 		
 		// if 있으면 돌려주기
@@ -39,7 +44,7 @@ public class ChatRoomService {
 			return chatroom.getId().toString();
 		}
 
-		else if(enterChatRoomRepository.countByUser_Id(user.getId())>=1) { //권한확인하기
+		else if(enterChatRoomRepository.countByUser_Id(user.getId())>=1 && user.getPermission().equalsIgnoreCase("ROLE_ADMIN")) { //권한확인하기
 			return "";
 		}
 
@@ -56,28 +61,33 @@ public class ChatRoomService {
 		}
 		
 	}
-
-
-	public List<ChatRoomDTO> getAllChatRoom() {
-		
-		List<ChatRoom> chatrooms = chatRoomRepository.findAll();
-		List<ChatRoomDTO> chatroomDTOs = new ArrayList<ChatRoomDTO>();
-		for(ChatRoom room : chatrooms) {
-			if(room.getIsDelete()==0) {
-				chatroomDTOs.add(ChatRoomDTO.toDTO(room));
-			}
-		}
-		
-		return chatroomDTOs;
-	}
-
-  
+	  
 	// chatroom.isActive(0)으로 바꾸기
 	public boolean closeChatRoom(String useremail) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+
+	public List<ChatRoomDTO> getAllChatRoom(String useremail) {
+		Users user = userRepository.findByEmail(useremail).get();
+		
+		if(user.getPermission().equalsIgnoreCase("ROLE_ADMIN")) {
+			List<ChatRoom> chatrooms = chatRoomRepository.findAll();
+			List<ChatRoomDTO> chatroomDTOs = new ArrayList<ChatRoomDTO>();
+			for(ChatRoom room : chatrooms) {
+				if(room.getIsDelete()==0) {
+					chatroomDTOs.add(ChatRoomDTO.toDTO(room));
+				}
+			}
+			return chatroomDTOs;
+		}
+		else
+			throw new IllegalArgumentException("권한이 없습니다.");
+		
+	}
+
+	
 
 	
 	
